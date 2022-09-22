041d16b03602127b43b8495ba28f45c973d703c1
Abby Nyakara <abbynyakara@gmail.com>
2022-09-22 11:31:28 +0300

    correct linter errors

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -9,10 +9,5 @@ describe Source do
     it 'Should create an instance of the source class' do
       expect(@source).to be_an_instance_of Source
     end
-
-    # it "Should add a source to the items array" do
-    #   @source.add_item("Source1")
-    #   expect(@source.items).not_to be_empty
-    # end
   end
 end

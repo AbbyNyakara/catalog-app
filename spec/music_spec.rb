292d61fe1ff0672cea156569d2bfec93220c5ae7
Abby Nyakara <abbynyakara@gmail.com>
2022-09-22 11:29:18 +0300

    correct linter errors

--- spec/music_spec.rb
+++ spec/music_spec.rb
@@ -5,12 +5,12 @@ describe MusicAlbum do
     @musicalbum = MusicAlbum.new(true, '2010-01-02')
   end
 
-  context "When testing the MusicAlbum class" do
-    it "Should create an instance of the class" do
+  context 'When testing the MusicAlbum class' do
+    it 'Should create an instance of the class' do
       expect(@musicalbum).to be_an_instance_of MusicAlbum
     end
 
-    it "Should allow the music album to be archived" do
+    it 'Should allow the music album to be archived' do
       expect(@musicalbum.can_be_archived?).to be_truthy
     end
   end
